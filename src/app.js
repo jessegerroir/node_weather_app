f5d55539c2ba2cc893031bc5f22d325f0b775e1c
Jesse Gerroir <jessegerroir@gmail.com>
2020-05-16 18:32:22 -0700

    Pages and Styling
    Finished setting up the pages, including header, footer, general site navigation, and then styling them all.

--- src/app.js
+++ src/app.js
@@ -1,22 +1,50 @@
 const path = require('path')
 const express = require('express')
-
-
-
+const hbs = require('hbs')
 
 // call express to create new application
 const app = express()
 
-// We create the path to the public folder from src
-// It's where all our web pages are
+// Define paths for express config
 const publicDirectory = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
-// We load this static public directory into express
-// It recognizes all the web pages in the static directory
-// and builds the routing by the names of the pages and their relation
-// to the index.html page
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory for express to serve
+// (It recognizes all the web pages in the static directory 
+// and builds the routing)
 app.use(express.static(publicDirectory))
 
+
+// Setup the path to the Index page (using handlebar)
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Jesse G.'
+    })
+})
+
+// Setup the path to the About page (using handlebar)
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Jesse G.'
+    })
+})
+
+// Setup the path to the Help page (using handlebar)
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        message: 'This is the help page.',
+        name: 'Jesse G.'
+    })
+})
 
 // These other routes determine what is served for the other pages
 
@@ -26,6 +54,23 @@ app.get('/weather', (req, res) => {
         location: 'Boston'
     }
     res.send(weather)
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404page', {
+        title: '404: Page Not Found',
+        name: 'Jesse G.',
+        errorMessage: 'Unable to find help article'
+    })
+})
+
+// generic 404 page to match anything that hasn't been matched above
+app.get('*', (req, res) => {
+    res.render('404page', {
+        title: '404: Page Not Found',
+        name: 'Jesse G.',
+        errorMessage: 'Unable to find the requested page.'
+    })
 })
 
 // starts server and has port listen at port 3000
